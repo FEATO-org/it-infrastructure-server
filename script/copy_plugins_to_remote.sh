984e2e5841f5e0c0c4a458fffb9728b3dda9995f
LenTakayama <teikailen@gmail.com>
2023-06-18 22:48:56 +0900

    リソースパックの追加や農業プラグインの設定、プラグイン系のボリューム改善

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -4,3 +4,4 @@ WORKDIR=$(pwd)
 cd "$(dirname "$0")"
 
 scp ../minecraft/plugins/downloads/* vps:~/src/it-infrastructure-server/minecraft/plugins/downloads
+scp ../minecraft/packs/* vps:~/src/it-infrastructure-server/minecraft/packs
