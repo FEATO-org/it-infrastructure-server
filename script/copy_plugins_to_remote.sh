e8c5b388f1eee86b856be2920d7a772203e6da5c
LenTakayama <teikailen@gmail.com>
2023-06-24 00:39:44 +0900

    フォルダ構成に追従

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 WORKDIR=$(pwd)
 cd "$(dirname "$0")"
 
-scp ../minecraft/plugins/downloads/* vps:~/src/it-infrastructure-server/minecraft/plugins/downloads
-scp ../minecraft/packs/* vps:~/src/it-infrastructure-server/minecraft/packs
+scp ../minecraft/plugins/downloads/*.jar vps:~/src/it-infrastructure-server/minecraft/plugins
+scp ../minecraft/plugins/Geyser-Spigot/packs/* vps:~/src/it-infrastructure-server/minecraft/plugins/Geyser-Spigot/packs
 scp ../minecraft/datapacks/* vps:~/src/it-infrastructure-server/minecraft/datapacks
