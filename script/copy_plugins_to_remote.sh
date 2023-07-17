11eee30ad841b26c5e0f8410206a04aab9b5c12c
LenTakayama <teikailen@gmail.com>
2023-07-17 22:13:16 +0900

    リソースパックが適用されない問題に対策

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -6,4 +6,3 @@ cd "$(dirname "$0")"
 scp ../minecraft/plugins/*.jar vps:~/src/it-infrastructure-server/minecraft/plugins
 scp ../minecraft/plugins/Geyser-Spigot/packs/* vps:~/src/it-infrastructure-server/minecraft/plugins/Geyser-Spigot/packs
 scp ../minecraft/datapacks/* vps:~/src/it-infrastructure-server/minecraft/datapacks
-scp ../minecraft/resourcepacks/* vps:~/src/it-infrastructure-server/minecraft/resourcepacks
