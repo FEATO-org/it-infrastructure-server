7645882ea0cef802c9b3bfd0c9db371061442847
LenTakayama <teikailen@gmail.com>
2023-08-05 21:37:45 +0900

    プラグイン設定を本体からProxyに移動

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -4,5 +4,5 @@ WORKDIR=$(pwd)
 cd "$(dirname "$0")"
 
 scp ../minecraft/plugins/*.jar vps:~/src/it-infrastructure-server/minecraft/plugins
-scp ../minecraft/plugins/Geyser-Spigot/packs/* vps:~/src/it-infrastructure-server/minecraft/plugins/Geyser-Spigot/packs
+scp ../minecraft-proxy/plugins/Geyser-Spigot/packs/* vps:~/src/it-infrastructure-server/minecraft-proxy/plugins/Geyser-Spigot/packs
 scp ../minecraft/datapacks/* vps:~/src/it-infrastructure-server/minecraft/datapacks
