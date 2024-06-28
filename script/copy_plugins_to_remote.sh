14fcef7d0dd506309b480b17e3b778786c084ec7
LenTakayama <teikailen@gmail.com>
2024-06-28 15:02:54 +0900

    リソースパック使わないため修正

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 
 # scp ../resources/minecraft/plugins/* root@vps:/extras/plugins
 scp ../resources/minecraft/datapacks/* root@vps:/extras/datapacks
-scp ../resources/minecraft/resourcepacks/* root@vps:/extras/resourcepacks
+# scp ../resources/minecraft/resourcepacks/* root@vps:/extras/resourcepacks
 scp ../resources/minecraft/texturepacks/* root@vps:/extras/texturepacks
