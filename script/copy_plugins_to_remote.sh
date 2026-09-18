b1abb8fa768031e228accc3da75aa7a0f6f95120
LenTakayama <teikailen@gmail.com>
2026-09-18 21:14:42 +0900

    MinecraftのPluginとData Packを確定構成に整理

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
-set -euxo pipefail
-WORKDIR=$(pwd)
-cd "$(dirname "$0")"
+#!/usr/bin/env bash
+set -Eeuo pipefail
+shopt -s nullglob
+cd "$(dirname "$0")/.."
 
-# scp ../resources/minecraft/plugins/* root@vps:/extras/plugins
-scp ../resources/minecraft/datapacks/* root@vps:/extras/datapacks
-# scp ../resources/minecraft/resourcepacks/* root@vps:/extras/resourcepacks
-scp ../resources/minecraft/texturepacks/* root@vps:/extras/texturepacks
+: "${REMOTE_HOST:?Set REMOTE_HOST to the deployment node SSH destination}"
+REMOTE_DEPLOY_ROOT="${REMOTE_DEPLOY_ROOT:-/opt/it-infrastructure-server}"
+# Keep old ignored Data Packs out of the curated mount used by the Swarm stack.
+datapacks=(minecraft/java/datapacks/*.zip)
+texturepacks=(resources/minecraft/texturepacks/*.mcpack)
+if (( ${#datapacks[@]} )); then
+  scp "${datapacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/minecraft/java/datapacks/"
+fi
+if (( ${#texturepacks[@]} )); then
+  scp "${texturepacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/resources/minecraft/texturepacks/"
+fi
