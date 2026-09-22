80b668452a09a80f1788a377c721006d740e8d1b
LenTakayama <49947878+LenTakayama@users.noreply.github.com>
2026-09-22 11:53:35 +0900

    Separate Java and Bedrock resource packs

--- script/copy_plugins_to_remote.sh
+++ script/copy_plugins_to_remote.sh
@@ -7,10 +7,17 @@ cd "$(dirname "$0")/.."
 REMOTE_DEPLOY_ROOT="${REMOTE_DEPLOY_ROOT:-/opt/it-infrastructure-server}"
 # Keep old ignored Data Packs out of the curated mount used by the Swarm stack.
 datapacks=(minecraft/java/datapacks/*.zip)
-texturepacks=(resources/minecraft/texturepacks/*.mcpack)
+java_resourcepacks=(resources/minecraft/resourcepacks/java/*.zip)
+bedrock_resourcepacks=(
+  resources/minecraft/resourcepacks/bedrock/*.mcpack
+  resources/minecraft/resourcepacks/bedrock/*.zip
+)
 if (( ${#datapacks[@]} )); then
   scp "${datapacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/minecraft/java/datapacks/"
 fi
-if (( ${#texturepacks[@]} )); then
-  scp "${texturepacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/resources/minecraft/texturepacks/"
+if (( ${#java_resourcepacks[@]} )); then
+  scp "${java_resourcepacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/resources/minecraft/resourcepacks/java/"
+fi
+if (( ${#bedrock_resourcepacks[@]} )); then
+  scp "${bedrock_resourcepacks[@]}" "${REMOTE_HOST}:${REMOTE_DEPLOY_ROOT}/resources/minecraft/resourcepacks/bedrock/"
 fi
